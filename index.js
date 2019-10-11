9157cc9f2c81b5e18de5856f3001ac4589885f38
itai rosenberg <djitai69@gmail.com>
2019-10-11 18:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-	if (string == string.toLowerCase()) 
+	if (string == string.toLowerCase())
    return  'i can\'t hear you!'
-  if (string == string.toUpperCase()) 
+  if (string == string.toUpperCase())
     return  'YES INDEED!'
 debugger;
 	if (string === "I love you, Grandma.")
